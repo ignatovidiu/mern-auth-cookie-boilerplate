0cfb06b4dfb4816a729d4f61678b1a6b956ea05c
Ovidiu Ignat <ignat.ovidiu84@gmail.com>
2026-03-24 19:27:55 +0200

    final code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,14 +3,25 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import { ToastContainer } from 'react-toastify';
+import Header from './components/Header';
+import PrivateRoute from './components/PrivateRoute';
+import Profile from './pages/Profile';
 
 const App = () => {
   return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/login' element={<Login />} />
-      <Route path='/register' element={<Register />} />
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='' element={<PrivateRoute />}>
+          <Route path='/profile' element={<Profile />} />
+        </Route>
+      </Routes>
+      <ToastContainer />
+    </>
   );
 };
 
--- client/src/store/store.js
+++ client/src/store/store.js
@@ -7,4 +7,7 @@ export const store = configureStore({
     auth: authReducer,
     [apiSlice.reducerPath]: apiSlice.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
+  devTools: true,
 });
